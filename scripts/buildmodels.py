8fe99f6f1fdb0a0383db067fc852428f2365115d
Renato Rocha Souza <rsouza.fgv@gmail.com>
2021-03-23 19:41:27 +0000

    adding script to run fastext

--- scripts/buildmodels.py
+++ scripts/buildmodels.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import re
+import time
 import pickle
 import pandas as pd
 import gensim
@@ -10,16 +11,8 @@ from gensim.models import Word2Vec
 
 import parser
 
-def timing(f):
-    @wraps(f)
-    def wrap(*args, **kw):
-        ts = time()
-        result = f(*args, **kw)
-        te = time()
-        print('func:{} took: {:2.4f} sec'.format(f.__name__, te-ts))
-        return result
-    return wrap
-
+df_files = pd.read_pickle('../outputs/df_files.pkl')
+folder = '/home/jovyan/shared/C_amc_141/R_amc_3.1_12921/203_vert_spacy_rftt/'
 
 def articles_reader(begin, end):
     responses = []
@@ -30,31 +23,38 @@ def articles_reader(begin, end):
         responses.extend(response)
     return responses
 
-
-if __name__ == "__main__":
-    df_files = pd.read_pickle('../outputs/df_files.pkl')
-    folder = '/home/jovyan/shared/C_amc_141/R_amc_3.1_12921/203_vert_spacy_rftt/'
-
-    embedding_size = 300
-    window_size = 8
-    min_word = 2
-    down_sampling = 1e-2
-
-
-model = FastText(responses,
-                 size=embedding_size,
-                 window=window_size,
-                 min_count=min_word,
-                 sample=down_sampling,
+def build_model(list_sentences, parameters,mode=FastText):
+    model = mode(list_sentences,
+                 size=parameters['embedding_size'],
+                 window=parameters['window_size'],
+                 min_count=parameters['min_word'],
+                 sample=parameters['down_sampling'],
                  workers = -1,
                  sg=1,
                  iter=100)
+    return model
 
+def update_model(model, list_sentences):
+    model.build_vocab(list_sentences, update=True)
+    model.train(list_sentences, total_examples=len(list_sentences), epochs=model.epochs)
+    return model
 
-model.build_vocab(responses, update=True)  # Update the vocabulary
-model.train(responses, total_examples=len(responses), epochs=model.epochs)
+if __name__ == "__main__":
 
+    parameters = {'embedding_size':300,
+              'window_size': 8,
+              'min_word': 3,
+              'down_sampling': 1e-2
+             }
+    
+    for idx in range(0,len(df_files),50):
+        print(f'Processing files from {idx} to {idx+50}')
+        articles = articles_reader(idx,idx+50)
+        if idx == 0:
+            model = build_model(articles, parameters)
+        else:
+            model = update_model(model, articles)
 
-model.save("../outputs/amc.fasttext.300.model")
+    model.save("../outputs/amc.fasttext.300.model")
 
 
